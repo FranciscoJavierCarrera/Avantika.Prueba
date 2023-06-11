f861acb7114b46c2df5582fe82190fdf8fa199f6
Francisco Javier Carrera <101222919+FranciscoJavierCarrera@users.noreply.github.com>
2023-06-10 19:23:53 -0600

    update

---
+++ NetMarket/Core/Entities/Producto.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Core.Entities
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public int Stock { get; set;}
+        public decimal Precio { get; set; }
+        public int Marca { get; set; }
+        public string? Imagen { get; set; }
+
+    }
+}
---
+++ NetMarket/Core/Interfaces/IProductoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductoRepository
+    {
+        Task<Producto> GetProductoByIdAsync(int id);
+        Task<IReadOnlyList<Producto>> GetAllProductsAsync();
+    }
+}
